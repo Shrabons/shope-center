f931a1032ee1d436bf7804d157205f65a2a31caa
shrabons <shrabon.developer@gamil.com>
2024-03-27 21:22:26 +0600

    all image action and show

--- components/product/ProductImages.jsx
+++ components/product/ProductImages.jsx
@@ -1,15 +1,22 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
 
 const ProductImages = ({ product }) => {
   const thumbnail = product.images.slice(-1)[0];
   const productImg = product.images.slice(0, -1);
-  const urlImg = thumbnail ?? product.thumbnail;
+  let urlImg = thumbnail ?? product.thumbnail;
+  const [imgchange, setImageChange] = useState(urlImg);
+
+  const handleImageChange = (url) => {
+    setImageChange(url);
+  };
 
   return (
     <div className="w-full lg:w-7/12 border border-slate-500/20 p-4">
       <Image
-        src={urlImg}
-        className="w-[400px] h-[500px] mx-auto object-cover"
+        src={imgchange}
+        className="w-[400px] h-[500px] mx-auto object-cover "
         width={400}
         height={500}
         alt=""
@@ -19,10 +26,11 @@ const ProductImages = ({ product }) => {
           <Image
             key={img}
             src={img}
-            className="w-[100px] h-[100px] mx-auto border object-cover"
+            className="w-[100px] h-[100px] mx-auto border object-cover cursor-pointer"
             width={100}
             height={100}
             alt="image"
+            onClick={() => handleImageChange(img)}
           />
         ))}
       </div>
